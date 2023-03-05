e25cb6cb45e4fc98dca0357aa15709702892e46e
Rewingle <kul.mehmetali@yahoo.com>
2023-03-06 02:53:49 +0300

    env

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import react from "react"
+
+export default function FourOhFour() {
+  return( 
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div>
+        <div><h1 style={{fontFamily:'monospace'}}>We can't find that page</h1></div>
+        <div>this page might not be exist</div>
+      </div>
+
+    </div>)
+}
